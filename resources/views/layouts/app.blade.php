0eea369afc8059ad7a167b674af3be35965b9e4d
huybo1410 <tuilahuy69@gmail.com>
2024-03-15 12:05:24 +0700

    Add_layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cửa hàng mắt kính</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+@include('layouts.header')
+
+
+@yield('content')
+   
+@include('layouts.footer')
+    
+</body>
+</html>
